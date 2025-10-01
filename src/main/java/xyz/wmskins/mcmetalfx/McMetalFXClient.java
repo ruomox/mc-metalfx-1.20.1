7c216bac91f81f657f4b40c0f8a0c62c24c4b001
Mox <mox@Moxs-MacBook-Pro.local>
2025-10-01 18:47:53 +0800

    Initial commit

---
+++ src/main/java/xyz/wmskins/mcmetalfx/McMetalFXClient.java
@@ -0,0 +1,8 @@
+package xyz.wmskins.mcmetalfx;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class McMetalFXClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
---
+++ src/main/java/xyz/wmskins/mcmetalfx/McMetalFXDataGenerator.java
@@ -0,0 +1,11 @@
+package xyz.wmskins.mcmetalfx;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class McMetalFXDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
